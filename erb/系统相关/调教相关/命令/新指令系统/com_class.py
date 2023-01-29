4920831265ad4116848c39b1493efd129369854f
xiaojiZack <879838984@qq.com>
2023-01-29 11:59:55 +0800

    打算切换到sugarcube Era框架

---
+++ erb/系统相关/调教相关/命令/新指令系统/com_class.py
@@ -0,0 +1,9 @@
+import erajs.api as a
+from erb.系统相关.口上相关.口上调用 import comkojo
+from erb.系统相关.调教相关.体力衰减 import sum_pp
+from erb.系统相关.调教相关.命令.下一回合 import singal_step
+from erb.系统相关.调教相关.命令.执行列表增减 import append_doing_list, check_doing_list, remove_doing_list
+
+class com(object):
+    def __init__(self,comid,comdifficult,comtag,comtype) -> None:
+        pass
